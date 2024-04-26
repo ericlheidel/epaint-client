bf9f41aa639cf3af67c9e83b75e5e663d67a9de7
Eric Heidel <ericleeheidel@yahoo.com>
2024-04-26 15:28:09 -0500

    User can now search and filter paints

---
+++ src/components/paints/PaintFilterBar.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react"
+
+export const PaintFilterBar = ({ setSearchText, setOrderBy }) => {
+  const [selectValue, setSelectValue] = useState("")
+
+  return (
+    <div className="flex flex-row justify-between mb-5">
+      <input
+        type="text"
+        placeholder="Search Paints..."
+        className=""
+        spellCheck={false}
+        onChange={(e) => {
+          setSearchText(e.target.value)
+        }}
+      />
+      <div>
+        {selectValue !== "" && (
+          <button
+            className="mr-2 test"
+            onClick={() => {
+              setSelectValue("")
+              setOrderBy("")
+            }}
+          >
+            X
+          </button>
+        )}
+
+        <select
+          value={selectValue}
+          onChange={(e) => {
+            setSelectValue(e.target.value)
+            setOrderBy(e.target.value)
+          }}
+        >
+          <option value={""} key={0}>
+            Order Paints By...
+          </option>
+          <option value={"color"} key={1}>
+            Paint Color
+          </option>
+          <option value={"paint_number"} key={2}>
+            Paint Number
+          </option>
+        </select>
+      </div>
+    </div>
+  )
+}
