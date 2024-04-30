22bb2248dd336f71ca53eb1b347709651c8a3bd2
Eric Heidel <ericleeheidel@yahoo.com>
2024-04-30 12:56:50 -0500

    user can add a payment via payments view

--- src/components/payments/Payments.jsx
+++ src/components/payments/Payments.jsx
@@ -1,8 +1,17 @@
 import { useEffect, useState } from "react"
-import { deleteUserPayment, getUserPayments } from "../../data/payments.jsx"
+import {
+  addNewPayment,
+  deleteUserPayment,
+  getUserPayments,
+} from "../../data/payments.jsx"
 
 export const Payments = () => {
   const [payments, setPayments] = useState([])
+  const [isHidden, setIsHidden] = useState(true)
+
+  const [newPaymentName, setNewPaymentName] = useState("")
+  const [newAcctNumber, setNewAcctNumber] = useState("")
+  const [newExDate, setNewExDate] = useState("")
 
   const refresh = () => {
     getUserPayments().then((res) => {
@@ -24,6 +33,18 @@ export const Payments = () => {
     if (confirmDelete) {
       deleteUserPayment(paymentId).then(refresh())
     }
+  }
+
+  const handleSubmit = () => {
+    const newPayment = {
+      newPaymentName,
+      newAcctNumber,
+      newExDate,
+    }
+
+    addNewPayment(newPayment).then(() => {
+      refresh()
+    })
   }
 
   return (
@@ -49,11 +70,89 @@ export const Payments = () => {
                       <i className="fa-solid fa-trash"></i>
                     </button>
                   </td>
+                  <td>
+                    <button
+                      className="ml-3"
+                      onClick={() => console.log("clicked")}
+                    >
+                      <i className="fa-solid fa-pen-to-square"></i>
+                    </button>
+                  </td>
                 </tr>
               )
             })}
           </tbody>
         </table>
+        <div>
+          <button
+            className="test"
+            hidden={!isHidden}
+            onClick={() => {
+              setIsHidden(false)
+            }}
+          >
+            Add Payment
+          </button>
+        </div>
+        <div className="add-payment-form" hidden={isHidden}>
+          <form onSubmit={handleSubmit}>
+            <fieldset>
+              <label>
+                Payment Name:{" "}
+                <input
+                  id="name"
+                  type="text"
+                  value={newPaymentName}
+                  onChange={(e) => {
+                    setNewPaymentName(e.target.value)
+                  }}
+                />
+              </label>
+            </fieldset>
+            <fieldset>
+              <label>
+                Account Number:{" "}
+                <input
+                  id="name"
+                  type="text"
+                  value={newAcctNumber}
+                  onChange={(e) => {
+                    setNewAcctNumber(e.target.value)
+                  }}
+                />
+              </label>
+            </fieldset>
+            <fieldset>
+              <label>
+                Expiration Date:{" "}
+                <input
+                  id="name"
+                  type="text"
+                  value={newExDate}
+                  onChange={(e) => {
+                    setNewExDate(e.target.value)
+                  }}
+                />
+              </label>
+            </fieldset>
+            <div className="flex flex-row justify-evenly mx-auto w-1/3">
+              <fieldset>
+                <button className="test">Add Payment</button>
+              </fieldset>
+              <fieldset>
+                <button
+                  className="test"
+                  onClick={(e) => {
+                    e.preventDefault()
+                    setIsHidden(true)
+                  }}
+                >
+                  Cancel
+                </button>
+              </fieldset>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   )
