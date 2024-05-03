b5ef50fb3725d0ffac7dfa32b92c15a7b3342b5a
Eric Heidel <ericleeheidel@yahoo.com>
2024-05-03 16:08:49 -0500

    i have styled add-payment

--- src/components/payments/AddPayment.jsx
+++ src/components/payments/AddPayment.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { addNewPayment } from "../../data/payments.jsx"
 import PropTypes from "prop-types"
+import { button, gradientOne } from "../../utils.jsx"
 
 export const AddPayment = ({ refresh, isNewHidden, setIsNewHidden }) => {
   const [newPaymentName, setNewPaymentName] = useState("")
@@ -40,66 +41,77 @@ export const AddPayment = ({ refresh, isNewHidden, setIsNewHidden }) => {
   }
 
   return (
-    <div className="add-payment-form" hidden={isNewHidden}>
-      <form onSubmit={handleSubmit}>
-        <fieldset>
-          <label>
-            Payment Name:{" "}
-            <input
-              id="paymentName"
-              type="text"
-              value={newPaymentName}
-              onChange={(e) => {
-                setNewPaymentName(e.target.value)
-              }}
-            />
-          </label>
-        </fieldset>
-        <fieldset>
-          <label>
-            Account Number:{" "}
-            <input
-              id="name"
-              type="text"
-              value={newAcctNumber}
-              onChange={(e) => {
-                setNewAcctNumber(e.target.value)
-              }}
-            />
-          </label>
-        </fieldset>
-        <fieldset>
-          <label>
-            Expiration Date:{" "}
-            <input
-              id="name"
-              type="text"
-              value={newExDate}
-              onChange={(e) => {
-                setNewExDate(e.target.value)
-              }}
-            />
-          </label>
-        </fieldset>
-        <div className="flex flex-row justify-evenly mx-auto w-1/3">
-          <fieldset>
-            <button type="submit" className="test">
-              Add Payment
-            </button>
+    <div
+      className="mt-36 flex justify-center items-center"
+      hidden={isNewHidden}
+    >
+      <div className={`${gradientOne} p-12 rounded-3xl shadow-2xl`}>
+        <form onSubmit={handleSubmit}>
+          <fieldset className="w-full mb-8">
+            <label htmlFor="paymentName" className="text-4xl mb-2 text-white">
+              Payment Name:{" "}
+              <input
+                id="paymentName"
+                type="text"
+                value={newPaymentName}
+                className="w-full p-4 text-3xl text-white bg-transparent border-b-2 border-white focus:outline-none focus:border-slate-400 transition duration-300 ease-in-out"
+                onChange={(e) => {
+                  setNewPaymentName(e.target.value)
+                }}
+              />
+            </label>
           </fieldset>
-          <fieldset>
-            <button
-              className="test"
-              onClick={(e) => {
-                e.preventDefault()
-                setIsNewHidden(true)
-              }}
+          <fieldset className="w-full mb-8">
+            <label htmlFor="accountNumber" className="text-4xl mb-2 text-white">
+              Account Number:{" "}
+              <input
+                id="accountNumber"
+                type="text"
+                value={newAcctNumber}
+                className="w-full p-4 text-3xl text-white bg-transparent border-b-2 border-white focus:outline-none focus:border-slate-400 transition duration-300 ease-in-out"
+                onChange={(e) => {
+                  setNewAcctNumber(e.target.value)
+                }}
+              />
+            </label>
+          </fieldset>
+          <fieldset className="w-full mb-8">
+            <label
+              htmlFor="expirationDate"
+              className="text-4xl mb-2 text-white"
             >
-              Cancel
-            </button>
+              Expiration Date:{" "}
+              <input
+                id="expirationDate"
+                type="text"
+                value={newExDate}
+                className="w-full p-4 text-3xl text-white bg-transparent border-b-2 border-white focus:outline-none focus:border-slate-400 transition duration-300 ease-in-out"
+                onChange={(e) => {
+                  setNewExDate(e.target.value)
+                }}
+              />
+            </label>
           </fieldset>
-        </div>
-      </form>
+          <div className="flex flex-row justify-evenly">
+            <fieldset>
+              <button type="submit" className={`${button}`}>
+                Add Payment
+              </button>
+            </fieldset>
+            <fieldset>
+              <button
+                className={`${button} ml-12`}
+                onClick={(e) => {
+                  e.preventDefault()
+                  setIsNewHidden(true)
+                }}
+              >
+                Cancel
+              </button>
+            </fieldset>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
