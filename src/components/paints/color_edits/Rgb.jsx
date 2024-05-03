80e996b70e939f00e9693674eea1ab9d4b08d4b4
Eric Heidel <ericleeheidel@yahoo.com>
2024-05-03 13:19:08 -0500

    I have split the Hex, RGB, and CMYK edit divs into their own components

---
+++ src/components/paints/color_edits/Rgb.jsx
@@ -0,0 +1,97 @@
+import PropTypes from "prop-types"
+import { useState } from "react"
+import { updatePaint } from "../../../data/paints.jsx"
+import {
+  button,
+  buttonNoMarginNoSize,
+  gradient,
+  input,
+} from "../../../utils.jsx"
+
+export const RgbEdit = ({ paint, paintId, getAndSetPaintAfterUpdate }) => {
+  const [rgbIsHidden, setRgbIsHidden] = useState(true)
+  const [updatedRgb, setUpdatedRgb] = useState("")
+
+  const handleUpdateRgb = (e) => {
+    e.preventDefault()
+
+    const rgbCodeFormat =
+      /^R(25[0-5]|2[0-4]\d|1\d{2}|[1-9]\d?)\sG(25[0-5]|2[0-4]\d|1\d{2}|[1-9]\d?)\sB(25[0-5]|2[0-4]\d|1\d{2}|[1-9]\d?)$/
+
+    const updatedRgbCode = {
+      id: paintId,
+      rgb: updatedRgb,
+    }
+
+    if (updatedRgb === "") {
+      window.alert("Please enter an RGB code...")
+    } else {
+      if (!rgbCodeFormat.test(updatedRgb)) {
+        window.alert("Please enter a valid RGB code...\ni.e. R255 G255 B255")
+      } else {
+        setRgbIsHidden(true)
+        updatePaint(updatedRgbCode).then(() => {
+          getAndSetPaintAfterUpdate(paintId)
+        })
+      }
+    }
+  }
+
+  return (
+    <>
+      <div className="mb-9">
+        {paint.rgb == null ? (
+          <button
+            hidden={!rgbIsHidden}
+            className={`${button} ml-5 mb-8 mt-6`}
+            onClick={() => setRgbIsHidden(false)}
+          >
+            Add an RGB Code
+          </button>
+        ) : (
+          `RGB: ${paint.rgb}`
+        )}
+      </div>
+      <div className="flex flex-col mr-5">
+        <form>
+          <input
+            hidden={rgbIsHidden}
+            type="text"
+            placeholder="Enter an RGB Code..."
+            value={updatedRgb}
+            className={`${input} ${gradient} mt-6`}
+            onChange={(e) => {
+              setUpdatedRgb(e.target.value)
+            }}
+          />
+          <div className="mt-4">
+            <button
+              hidden={rgbIsHidden}
+              className={`${buttonNoMarginNoSize} p-4`}
+              onClick={handleUpdateRgb}
+            >
+              Submit
+            </button>
+            <button
+              hidden={rgbIsHidden}
+              className={`${buttonNoMarginNoSize} p-4 ml-5`}
+              onClick={(e) => {
+                e.preventDefault()
+                setUpdatedRgb("")
+                setRgbIsHidden(true)
+              }}
+            >
+              Cancel
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  )
+}
+
+RgbEdit.propTypes = {
+  paint: PropTypes.object.isRequired,
+  paintId: PropTypes.string.isRequired,
+  getAndSetPaintAfterUpdate: PropTypes.func.isRequired,
+}
